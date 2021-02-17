25ecc969c894e4dbe0832b53eea189d15d87a119
fukiran <macdorphoto@yahoo.com>
2021-02-17 16:45:52 +0000

    first fight

--- src/main/java/rooms/Room.java
+++ src/main/java/rooms/Room.java
@@ -1,4 +1,29 @@
 package rooms;
 
+import avatar.Avatar;
+import enemy.Enemy;
+import warriors.Dwarf;
+
 public class Room {
+    private Avatar hero;
+    private Enemy enemy;
+
+    public Room(Avatar hero, Enemy enemy) {
+        this.hero = hero;
+        this.enemy = enemy;
+    }
+
+    public Enemy getEnemy() {
+        return this.enemy;
+    }
+
+
+    public Avatar getHero() {
+        return this.hero;
+    }
+
+    public void attack(Dwarf hero, Enemy enemy) {
+        int damagePoints = hero.attack();
+        enemy.removeHealthPoints(damagePoints);
+    }
 }
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -1,5 +1,49 @@
+import enemy.Enemy;
+import enemy.EnemyType;
+import org.junit.Before;
+import org.junit.Test;
+import rooms.Room;
+import warriors.Dwarf;
+import warriors.WeaponType;
+
+import static org.junit.Assert.assertEquals;
+
 public class RoomTest {
+
+    Room room;
+    Dwarf dwarf;
+    Enemy enemy;
+
+    @Before
+    public void before(){
+        dwarf = new Dwarf("Hamlett", 100, 10, WeaponType.AXE);
+        enemy = new Enemy("Donald", 100, 1, EnemyType.POLITICIAN);
+        room = new Room(dwarf, enemy);
+
+    }
+
+
+@Test
+public void hasEnemy(){
+    assertEquals( enemy,  room.getEnemy() );
 }
 
 
-//
+@Test
+public void hasHero(){
+        assertEquals( dwarf, room.getHero());
+
+}
+
+@Test
+ public void heroCanDamage(){
+        room.attack(dwarf, enemy);
+        assertEquals(91, enemy.getHealthPoints());
+}
+
+
+
+// canHeal
+
+}
+
